4ee6a8bc0e5864b417f6cd39361d299d6b78cdb1
VinayTS <vinay@succorfish.com>
2021-05-23 13:21:36 +0530

    Starting the Service. Stopping the Service in the Service class. Checking which Thread will be used to start and stop the Service.

--- Code/app/src/main/java/com/example/services/MainActivity.java
+++ Code/app/src/main/java/com/example/services/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.services;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String APPLICATION_TAG=MainActivity.class.getSimpleName();
+
+    private static final String COMMON_TAG="COMMON_TAG "+APPLICATION_TAG;
+    private static final String TAG=COMMON_TAG;
+    
+    
+    /**
+     * Android always recomends to use explicit intent to start the Service.
+     */
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intializeService();
+    }
+
+    private void intializeService() {
+        serviceIntent=new Intent(this,MyServiceDemo.class);
+    }
+
+    public void startService(View view) {
+        Log.d(TAG, "startService: Thread ID= "+Thread.currentThread().getId());
+            startService(serviceIntent);
+    }
+
+    public void stopService(View view) {
+        Log.d(TAG, "stopService: ");
+          //  stopService(serviceIntent);
     }
 }
+
+
+//Notes link:-https://drive.google.com/drive/folders/1j8WkvIWXHNVFSp1dNl0i7hCj1fHrDep-?usp=sharing
---
+++ Code/app/src/main/java/com/example/services/MyServiceDemo.java
@@ -0,0 +1,46 @@
+package com.example.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MyServiceDemo extends Service {
+
+
+    private static final String APPLICATION_TAG=MyServiceDemo.class.getSimpleName();
+    private static final String COMMON_TAG="COMMON_TAG "+APPLICATION_TAG;
+    private static final String TAG=COMMON_TAG;
+
+    /**
+     *This method is depricated.
+     */
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    /**
+     * This method will get executed when ever we start the Service.
+     */
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: "+Thread.currentThread().getId());
+        stopSelf();//used to stop the Service.
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: "+Thread.currentThread().getId());
+    }
+}
